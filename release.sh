015453717ec3dc8fb5c7f35442ee0d86f29c9405
Jay Fong <fjc0kb@gmail.com>
2019-08-02 15:26:50 +0800

    fix: release

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 NPC_VERSION=$(grep -o "NPC_VERSION=.*" Dockerfile | sed "s/NPC_VERSION=//g")
 
-git tag "$NPC_VERSION"
+git tag -a "$NPC_VERSION" -m "npc v$NPC_VERSION"
 git push origin --follow-tags
